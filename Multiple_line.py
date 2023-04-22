fc6c7458ff44ec607b97bfa7e9c0dba21c152238
unknown <arixxdelera21@gmail.com>
2023-04-22 18:36:34 +0800

    def multiple_line and mylife.txt has been created

--- Multiple_line.py
+++ Multiple_line.py
@@ -9,7 +9,10 @@
 # Are there more lines y/n? n 
 
 # Make a function def method
-# Make a file called "mylife.txt" and give it permission to be written to with the "write" command. 
+def multiple_line():
+    # Make a file called "mylife.txt" and give it permission to be written to with the "write" command. 
+    with open('mylife.txt', 'w') as input_file:
+
 # Loop over the code using a while statement.
 # The "write" method of the file object will be used to save the line of text to the file, followed by a newline character (\n).
 # Ask if the user wants to add more lines by asking y/n.
