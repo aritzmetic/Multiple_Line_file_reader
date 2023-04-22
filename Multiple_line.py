e47d7154e4bf8228304fe4888449b9b42531e1d2
unknown <arixxdelera21@gmail.com>
2023-04-22 18:26:41 +0800

    pseudocode has been created

---
+++ Multiple_line.py
@@ -0,0 +1,17 @@
+# Delera, Aritz B.
+
+# Write a method in python to write multiple line of text contents into a text file mylife.txt. See sample output:
+# Enter line: Beautiful is better than ugly.
+# Are there more lines y/n? y
+# Enter line: Explicit is better than implicit.
+# Are there more lines y/n? y
+# Enter line: Simple is better than complex.
+# Are there more lines y/n? n 
+
+# Make a function def method
+# Make a file called "mylife.txt" and give it permission to be written to with the "write" command. 
+# Loop over the code using a while statement.
+# The "write" method of the file object will be used to save the line of text to the file, followed by a newline character (\n).
+# Ask if the user wants to add more lines by asking y/n.
+# If n, then break the code.
+# call the method to run the code
